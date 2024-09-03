711957617c3ccff1382e68f5d5338cfd899ab5f4
jcontreras <jcontreras@appm.com.ar>
2024-09-03 09:26:35 -0300

    APP001 modelos y migraciones de: post, comment, img, cat, subcat,

---
+++ database/migrations/2024_09_03_100716_create_post_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('post', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable(); // luego se le asigna la relación
+            $table->double('lat');
+            $table->double('lng');
+            $table->text('comment')->nullable();
+            $table->boolean('private')->default(false);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('post');
+    }
+};
