ef6e45ed0e653ba344103fe813d68da846295e57
Karen Apablaza <kapablaza@puerto.appm.com.ar>
2024-10-18 14:58:52 -0300

    trabajo con likes de comenntarios

--- app/Http/Controllers/api/ApiCommentLikeController.php
+++ app/Http/Controllers/api/ApiCommentLikeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
 use App\Models\PostComment;
 use App\Models\PostCommentLike;
 use Illuminate\Http\Request;
@@ -11,20 +12,23 @@ class ApiCommentLikeController extends Controller
 {
     public function store(PostComment $postComment, Request $request)
     {
-        $request->validate([
-            'post_comment_id' => 'required|exists:post_comments,id',
-        ]);
-
-        $like = $postComment->likes()->create([
+        // $request->validate([
+        //     'post_comment_id' => 'required|exists:post_comments,id',
+        // ]);
+        PostCommentLike::firstOrCreate([
             'user_id' => auth()->id(),
+            'post_comment_id' => $postComment->id,
         ]);
+        // $like = $postComment->likes()->create([
+            // 'user_id' => auth()->id(),
+        // ]);
 
-        return response([], 201);
+        return response(new PostResource($postComment->post), 201);
     }
 
     public function destroy(PostComment $postComment, PostCommentLike $postCommentLike, Request $request)
     {
         $postCommentLike->delete();
-        return response([], 200);
+        return response($postComment, 200);
     }
 }
