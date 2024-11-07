e3a94753b3069bc13685b57e1748390790b81753
Karen Apablaza <kapablaza@puerto.appm.com.ar>
2024-11-07 11:56:55 -0300

    Mejora en el Post show - se saco el id y el boton de eliminar

--- resources/js/Pages/Post/Show.vue
+++ resources/js/Pages/Post/Show.vue
@@ -30,37 +30,45 @@ const emit = defineEmits(['showPostOnModal', 'deletePost']);
             </div>
             <div class="d-flex justify-content-between">
                 <div>
-                    <b># {{ post.id }}</b> <br>
+                    <h3 class="d-flex nav-link">
+                        <span v-if="post.category?.name"><b> #{{ post.category?.name }}</b>&nbsp;</span>
+                        <span v-if="post.subcategory?.name"><b> #{{ post.subcategory.name }} </b></span>
+                    </h3>
                     <i class="fas fa-calendar-alt"></i> 
                     {{ new Date(post.created_at).toLocaleDateString() }} 
                     <!-- {{ post.valid_until ? ' - ' + post.valid_until : '' }} -->
-                </div>               
-                <div v-if="post.user_id == $page.props.auth?.user?.id">
-                    <i role="button" title="Borrar" class="fas fa-trash-alt text-danger" @click="emit('deletePost', post)"></i>
-                </div>
+                </div>   
+                
+                <div class="dropdown" v-if="post.user_id == $page.props.auth?.user?.id">
+                    <a  data-bs-toggle="dropdown" href="#" aria-expanded="false">
+                        <i class="fas fa-ellipsis-h"></i>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="#" class="dropdown-item link-underline link-underline-opacity-0" @click="emit('deletePost', post)"><i role="button" title="Borrar" class="fas fa-trash-alt text-danger"></i> Eliminar reporte</a>
+                        </li>
+                    </ul>
+                </div>           
             </div>
-            <div v-if="post.location">
+            <div v-if="post.location" class="mb-2">
                 <i class="fas fa-map-marker-alt"></i>
                 {{ post.location }}
             </div>
-            <div v-if="post.comment">
+            <!-- <div v-if="post.comment">
                 <hr>
                 <span class="text-info">Esto es temporal:</span>
                 <p v-html="post.comment"></p>
                 <hr>
             </div>
-                <h5 class="d-flex nav-link">
-                <span v-if="post.category?.name"><b> #{{ post.category?.name }}</b>&nbsp;</span>
-                <span v-if="post.subcategory?.name"><b> #{{ post.subcategory.name }} </b></span>
-            </h5>
+             -->
             <div v-for="image in post.images" class="mb-2">
                 <a href="#" @click.prevent="$emit('showPostOnModal', post)" data-bs-toggle="modal" data-bs-target="#modalShowPost">
                     <img :src="image.file" alt="Imagen" class="img-fluid w-30"/>
                 </a>
             </div>
-
+            
             <Like :post="post" v-if="$page.props.auth?.user" />
-           
+            
             <Create :post="post" v-if="$page.props.auth?.user"></Create>
         </div>
     </div>
