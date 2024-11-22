92890b0ff323588cbe6ae1103dc95f40a6d1e299
Karen Apablaza <kapablaza@puerto.appm.com.ar>
2024-11-22 14:26:18 -0300

    Falta terminar post-create para subir archivos

--- resources/js/Pages/Post/Create.vue
+++ resources/js/Pages/Post/Create.vue
@@ -5,6 +5,7 @@ import MapaLatLng from '@/Components/MapaLatLng.vue';
 
 const WIDTH = 1280;
 const HEIGHT = 720;
+const fileName = ref('');  // Variable para almacenar el nombre del archivo
 const apiKey = import.meta.env.VITE_MAPBOX_TOKEN;
 const sending = ref(false)
 const props = defineProps({
@@ -71,6 +72,7 @@ const activeCategory = ref(null);
 const onFileChange = (event) => {
     const selectedFile = event.target.files[0];
     if (selectedFile) {
+        fileName.value = selectedFile.name; // Actualiza el nombre del archivo
         const reader = new FileReader();
         reader.onload = (e) => {
             imageSrc.value = e.target.result;
@@ -139,45 +141,6 @@ const urlToFile = (url) => {
     return file;
 }
 
-// Metodo para enviar el formulario
-// function funcionDeSubmit_old() {
-//     if(form.subcategory_id === null){
-//         Swal.fire({
-//             'icon' : 'error',
-//             'title' : 'Debe seleccionar una categoría y una subcategoría',
-//             'toast': true,
-//             'position': 'top-end',
-//             'timer': 2500,
-//             'timerProgressBar' : true,
-//             'showConfirmButton' : false
-//         });
-//         return;
-//     }
-
-//     if(form.image === null){
-//         Swal.fire({
-//             'icon' : 'error',
-//             'title' : 'Debe seleccionar una imagen',
-//             'toast': true,
-//             'position': 'top-end',
-//             'timer': 2500,
-//             'timerProgressBar' : true,
-//             'showConfirmButton' : false
-//         });
-//         return;
-//     }
-
-//     form.post(route('posts.store'));
-//     activeCategory.value = null;
-//     imageSrc.value = null;
-//     form.image = null;
-//     form.
-//     fullAddress = null;
-//     form.latitud = 0;
-//     form.longitud = 0;
-//     form.subcategory_id = null;
-//     document.getElementById('image').value = '';    
-// }
 const funcionDeSubmit = () => {
     if (form.subcategory_id === null) {
         Swal.fire({
@@ -240,11 +203,10 @@ function toggleCategory(id){
 }
 
 </script>
-
 <template>
     <!-- Crear un Post -->    
     <div id="c"></div>
-    <div class=" card w-100">
+    <div class="card w-100">
         <div class="card-body">
             <div v-if="categorias.length === 0 || cantidadSubcategorias === 0">
                 <div class="alert alert-danger">
@@ -257,67 +219,124 @@ function toggleCategory(id){
                 <div class="row">
                     <div class="fs-4">Nuevo reporte</div>
                     <p>Seleccione la categoria y subcategoria del posteo <i class="fas fa-carret-down"></i> </p>
-                    <!-- <p for="formFile" class="form-label mr-4">Seleccione la categoria y subcategoria del posteo</p> -->
-                    <div v-for="category in categorias" :key="category.id" class="col-12 col-md-4 mb-3">
-                        <!-- Boton para elegir las categorias -->
-                        <button type="button" class="btn shadow border-2 rounded-4 w-100" :class="{
+
+                    <!-- Categorías -->
+                    <div v-for="category in categorias" :key="category.id" class="col-12 col-md-4 mb-1">
+                        <button type="button" class="btn shadow pb-0 rounded-4 w-100" :class="{
                             'bg-success' : category.id === activeCategory,
-                            // 'bg-secondary' : subcategory.id != form.subcategory_id
                         }" @click="toggleCategory(category.id)">
-                        <!-- Category icon and name -->
-                        <span v-if="!activeCategory" v-html="category.icon" class="fs-3"></span>
-                        <p class="fs-4">{{ category.name }}</p>
-                    </button>
-                    
-                    <!-- Subcategorías -->
-                    <div v-if="activeCategory === category.id " class="mt-1">
-                        <!-- Mensaje si no hay subcategorías -->
-                        <div v-if="category.subcategories.length === 0" class="alert alert-warning">
-                            <div class="alert-body">
+                            <span v-if="!activeCategory" v-html="category.icon" class="fs-3"></span>
+                            <p class="fs-4">{{ category.name }}</p>
+                        </button>
+                    </div>
+
+                    <!-- Subcategorías (se muestra fuera del bloque de categoría activa) -->
+                    <div v-if="activeCategory !== null" class="mt-3">
+                        <div v-for="category in categorias" :key="category.id" v-show="category.id === activeCategory">
+                            <div v-if="category.subcategories.length === 0" class="alert alert-warning">
                                 No tiene subcategorías
                             </div>
+                            <div v-else>
+                                <button type="button" v-for="subcategory in category.subcategories" :key="subcategory.id"
+                                class="btn rounded-4 mr-2 mb-2"
+                                :class="{
+                                    'btn-success' : subcategory.id == form.subcategory_id,
+                                    'btn-outline-success' : subcategory.id != form.subcategory_id
+                                }"
+                                @click="form.subcategory_id = subcategory.id">
+                                    <span v-html="subcategory.icon"></span> 
+                                    {{ subcategory.name }}
+                                </button>
+                            </div>
                         </div>
-                        <ul class="list-group" v-else>
-                            <li v-for="subcategory in category.subcategories" :key="subcategory.id"
-                            class="list-group-item rounded-4"
-                            :class="{
-                                'bg-success' : subcategory.id == form.subcategory_id,
-                                // 'bg-secondary' : subcategory.id != form.subcategory_id
-                            }"
-                            role="button"
-                            @click="form.subcategory_id = subcategory.id"
-                            >
-                            <span v-html="subcategory.icon"></span> 
-                            {{ subcategory.name }}
-                        </li>
-                    </ul>
+                    </div>
                 </div>
-            </div>
+
+                <!-- Inputs adicionales como la imagen y el mapa -->
+                <!-- <input type="file" class="form-control border-2 p-2 rounded-5 mb-1" id="image"
+                name="image" capture="environment" accept="image/png, image/jpeg"
+                @change="onFileChange"> -->
+
+                     <div class="file-input-container">
+        <label for="file-upload" class="file-upload-label">
+            <i class="fas fa-camera"></i> <!-- Icono de cámara de Font Awesome -->
+            <div class="file-upload-text">Imagen</div>
+        </label>
+        <input type="file" id="file-upload" class="file-upload-input" @change="onFileChange" accept="image/*">
+        <!-- Mostrar el nombre del archivo seleccionado -->
+        <div v-if="fileName" class="file-name ml-1 d-none d-md-block">{{ fileName }}</div>
+    </div>
+                <div class="mb-1">
+                    <MapaLatLng :lat="form.latitud" :lng="form.longitud" @update:lat="(lat) => {form.latitud = lat; geocoding()}" @update:lng="(lng) => {form.longitud = lng}" > </MapaLatLng>
+                </div>
+                
+                <div v-if="searchingGeocode" class="d-flex mb-3 ml-2">
+                    <i class="fas fa-spinner fa-spin"></i>
+                </div>
+                <div v-else class="mb-3">
+                    <span>{{ form.fullAddress }}</span>
+                </div>
+
+                <button class="btn btn-primary rounded-4" :disabled="(form.subcategory_id == null && form.image == null) || sending">
+                    <span>
+                        <i v-if="sending" class="fas fa-circle-notch fa-spin"></i>
+                        <span v-else>Guardar</span>
+                    </span>
+                </button>
+            </form>
         </div>
-        
-        <input type="file" class="form-control border-2 p-2 rounded-5 mb-1" id="image"
-        name="image" capture="environment" accept="image/png, image/jpeg"
-        @change="onFileChange">
-        <div class="mb-3">
-            <MapaLatLng :lat="form.latitud" :lng="form.longitud" @update:lat="(lat) => {form.latitud = lat; geocoding()}" @update:lng="(lng) => {form.longitud = lng}" > </MapaLatLng>
-        </div>
-        <div v-if="searchingGeocode" class="d-flex mb-3 ml-2">
-            <i class="fas fa-spinner fa-spin"></i>
-        </div>
-        <input type="text" class="form-control border-2 p-2 rounded-5 mb-4" id="contenido" readonly v-show="form.fullAddress" v-model="form.fullAddress" required>
-        
-        <!-- Boton para postear -->
-        <button class="btn btn-primary rounded-4" :disabled="(form.subcategory_id == null && form.image == null) || sending">
-            <span>
-                <i v-if="sending" class="fas fa-circle-notch fa-spin"></i>
-                <span v-else>Guardar</span>
-            </span>
-        </button>
-        
-        <!-- <button type="button" @click="testearPusher">TEST PUSHER</button> -->
-        
-        
-    </form>
-</div>
-</div>
+    </div>
 </template>
+
+<style>
+.file-input-container {
+    position: relative;
+    display: inline-block;
+}
+
+.file-upload-label {
+    display: inline-block;
+    padding: 20px;
+    border: 2px dashed;
+    border-radius: 10px;
+    background-color: transparent;
+    cursor: pointer;
+    text-align: center;
+    width: 200px;
+    font-size: 20px;
+    color: #198754;
+    transition: all 0.3s ease;
+}
+
+/* Modo claro */
+.file-upload-label.bg-light {
+    background-color: #f8f9fa;
+    border-color: #59ff00;
+    color: #007bff;
+}
+
+.file-upload-label.bg-light i {
+    color: #007bff;
+}
+
+/* Modo oscuro */
+.file-upload-label.bg-dark {
+    background-color: #343a40;
+    border-color: #007bff;
+    color: #fff;
+}
+
+.file-upload-label.bg-dark i {
+    color: #fff;
+}
+
+.file-upload-label:hover {
+    background-color: #e9ecef;
+}
+
+/* El input real está oculto */
+.file-upload-input {
+    display: none;
+}
+</style>
+
